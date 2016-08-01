3efdd9307e5ab0d3b8a9ae1cf13f26d55b46b9f0
Larry Garfield <larry@garfieldtech.com>
2016-08-01 12:43:41 -0500

    Add Link and LinkCollection.

---
+++ src/LinkCollectionInterface.php
@@ -0,0 +1,28 @@
+<?php
+namespace Psr\Http\Link;
+
+/**
+ *
+ */
+interface LinkCollectionInterface
+{
+    /**
+     * Returns a collection of LinkInterface objects.
+     *
+     * The collection may be an array or any PHP \Traversable object. If no links
+     * are available, an empty array or \Traversable MUST be returned.
+     *
+     * @return LinkInterface[]|\Traversable
+     */
+    public function getLinks();
+
+    /**
+     * Returns a collection of LinkInterface objects that have a specific relationship.
+     *
+     * The collection may be an array or any PHP \Traversable object. If no links
+     * with that relationship are available, an empty array or \Traversable MUST be returned.
+     *
+     * @return LinkInterface[]|\Traversable
+     */
+    public function getLinksByRel($rel);
+}
